86e4b185cc18b89802b396ed0efd87b61fee7fbd
Tiia <tiia.husa@tuni.fi>
2024-07-19 14:11:48 +0300

    Part -osaa täydennetty 1d asti

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,219 +1,170 @@
-/*/import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//Komponentin määritys, voidaan käyttää ohjelmassa, AINA isolla  alkukirjaimella
+const Hello = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App/*/
-// Koodin yksinkertaistus
-
-/*/
-const App = //Sijoitetaan alempi komponentti muuttujaan App
-() => ( // Ja tästä alkaa ko. komponentti
-  <div>
-    <p>Hello world</p>
-  </div>
-) // Ja tähän riviin se loppuu
-
-export default App
-
-
-const App = () => {
-  console.log('Hello from komponentti') // Selaimen konsoliin tulee lisäteksti
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world xxx!</p>
     </div>
   )
 }
 
-
-
-
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+//Toinen komponentti, jolle välitetään dataa
+const Hello2 = (props) => {
+  //Voidaan poimia propsit myös muuttujiin, jos niitä vaikka tarvitsee käyttää koodissa useamman kerran
+  // Helppo poimia kaikki samalla kertaa
+  const {name, age, day } = props
+  //Komponentti komponentin sisällä, käytetty kompaktimpaa kirjoitustapaa, koska vain yksi komento
+  const bornYear = () => new Date().getFullYear() - age
 
   return (
-    <div> // Koodi näyttää HTML:ltä mutta on todellisuudessa JSX:ää
+    <div>
+      <p>Hello world {name}, {age} vuotta ja {day} päivää!</p>
+      <p>Olet todennäköisesti syntynyt vuonna {bornYear()}</p>    
+    </div>
+  )
+}
+
+//Kolmas komponentti, jolle välitetään dataa
+//Muuttujat nimetty jo komponentin luonnissa destruktoinnin avulla
+const Hello3 = ({name, age, day }) => {
+  //Komponentti komponentin sisällä, käytetty kompaktimpaa kirjoitustapaa, koska vain yksi komento
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>Hello worldie {name}, {age} vuotta ja {day} päivää!</p>
+      <p>Olet todennäköisesti syntynyt vuonna {bornYear()}</p>    
+    </div>
+  )
+}
+
+//Luodan komponentti Display
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+//Komponentti button
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+//Komponentin tila vaihtuu propsin tietojen perusteella
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+import { useState } from 'react'
+
+//Sovelluksen juurikomponentti App
+const App = () => {
+  // älä määrittele komponenttia täällä APPin sisällä vaan ulkopuollella!
+  //const Display = props => <div>{props.value}</div>
+
+
+  // Tässä on hyvä tapa käyttää sivun päivittämistä
+  //Huomaa ylempänä oleva import joka kuuluu tähän myös
+  
+  const [counter, setCounter] = useState(0)
+  const [ counter2, setCounter2 ] = useState(0)
+
+  //Buttonin funktiot voi määritellä joko buttonissa itsessään tai tällee erikseen App:in sisällä:
+  const increaseByOne = () => setCounter2(counter2 + 1)
+  const decreaseByOne = () => setCounter2(counter2 - 1)
+  const setToZero = () => setCounter2(0)
+  /*setTimeout(
+    () => setCounter(counter+1), 1000
+  )*/
+
+  //Taulukko, joka muistaa näppäimen painallukset
+  const [left, setLeft] = useState(0) //eli alkutila sulkujen sisään
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  const now = new Date()
+  const a = 5
+  const b = 4
+  const friends = [
+    {name : "Martta", age: 10, day: 100},
+    {name : "Kaapo", age: 50, day: b}
+  ]
+  //Seuraava komento toimii debuggaamisessa:
+  //debugger
+  console.log("päivä ", now, a+b) //tulostetaan selaimen konsoliin, elementit erotetana pilkulla
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  return (
+    <div>
+      <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+      </div>
+
+      <Display counter={counter}/>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}> 
+        zero
+      </button>
+
+      <Display counter={counter2}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
+      <Hello /> 
+      <Hello2 name = "Tiia" age = {100+1} day={a}/>
+      <Hello2 name = {friends[0].name} age = {friends[0].age} day = {friends[0].day} />
+      <Hello3 name = {friends[1].name} age = {friends[1].age} day = {friends[1].day} />
       <p>Hello world, it is {now.toString()}</p>
       <p>
         {a} plus {b} is {a + b}
       </p>
     </div>
   )
-} 
-
-const Hello = (props) => { // Määritetään Hello-funktiolle parametri name, eli "propsi"
-  return (
-    <div>
-      <p>Hello {props.name}</p> 
-    </div>
-  )
 }
 
-const App = () => { // Voidaan käyttää Hello-funktiota "pääfunktio" Appissa
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Tiia"/>
-      <Hello name="Jarkko"/>
-      <Hello />
-      <Hello />
-    </div>
-  )
-} 
-
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
-const App = () => { // Voidaan määritellä muuttujia myös pääfunktion alkuun
-  const nimi = 'Pekka'
-  const ika = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </div>
-  )
-} 
-
-
-
-const App = () => { // Ilman DIV-elementtiä ei App-komponenttia saa toimimaan
-  return (
-    <h1>Greetings</h1>
-    <Hello name="Maya" age={26 + 10} />
-    <Footer />
-  )
-} 
-
-
-
-const App = () => { // Mutta esim taulukollinen elementtejä ([&]) sopii
-  return [
-    <h1>Greetings</h1>,
-    <Hello name="Maya" age={26 + 10} />,
-  ]
-} 
-
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
-const App = () => { // Tai voidaan käyttää fragmentteja, eli ympäröidään
-  //komponentit tyhjällä elementillä (<> & </>) ja tällöin sovelluksen
-  //DOM-puuhun ei tule "ylimääräisiä elementtejä"
-  const name = 'Pekka'
-  const age = 10
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
-  )
-} 
-
-const App = () => { // Kun ohjelmaa yrittää kääntää, konsoli sanoo seuraavaa:
-  //react-dom.development.js:14887 Uncaught Error: Objects are not valid 
-  //as a React child (found: object with keys {name, age}). If you meant
-  // to render a collection of children, use an array instead.
-  const friends = [
-    { name: 'Leevi', age: 4 },
-    { name: 'Venla', age: 10 },
-  ]
-
-  return (
-    <div>
-      <p>{friends[0]}</p>
-      <p>{friends[1]}</p>
-    </div>
-  )
-} 
-
-//Edellien ei kääntynyt, koska Reactissa yksittäisten aaltosulkeissa olevien
-//renderöitävien asioiden tulee olla lukuja tai merkkijonoja
-
-//Korjataan asia seuraavasti:
-const App = () => {
-  const friends = [
-    { name: 'Leevi', age: 4 },
-    { name: 'Venla', age: 10 },
-  ]
-
-  return (
-    <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-    </div>
-  )
-} // Eli renderöidään erikseen nimi {friends[0].name} {friends[0].age} ja ikä
-//Ja nyt koodi toimii
-
-
-/*/
-
-// React sallii myös taulukon renderöimisen, jos taulukon sisältö on sallittua
-// Mutta palataan sen hyödyntämiseen myöhemmin
-const App = () => {
-  const friends = [ 'Leevi', 'Venla']
-
-  return (
-    <div>
-      <p>{friends}</p>
-    </div>
-  )
-}
-
-
-
+//Tää pitää aina olla alhaalla, älä poista
 export default App
--- osa1/part1/src/main.jsx
+++ osa1/part1/src/main.jsx
@@ -1,18 +1,30 @@
-/*/import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)/*/
-
-// Koodin yksinkertaistus
 import ReactDOM from 'react-dom/client'
 
-import App from './App2'
+import App from './App'
 
-// Renderöi komponentin sisällön tiedoston index.html määrittelemään div-elementtiin, jonka is:n arvona on root
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+/*
+Annetaan mainissa muuttuja counter ja lähetetään se root:ille, jolloin const App(counter) tulee olla muuttuja sisällään
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// Tässä luodaan funktio, joka kutsuu sivun renderöintiä
+const refresh = () => {
+  root.render(<App counter={counter} />)
+}
+//Ja tässä itse kutsuminen
+  refresh()
+  counter += 1
+  refresh()
+  counter += 1
+  refresh()*/
+
+//Renderöidään (eli päivitetään) sivu 1000 ms välein ja counter arvo kasvaa aina yhdellä
+//Ei hyvä tapa päivittää komponentteja
+/*setInterval(() => {
+    refresh()
+    counter += 1
+  }, 1000)*/
+
