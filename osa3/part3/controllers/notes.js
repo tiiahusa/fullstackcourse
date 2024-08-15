686a248344165d799188892b90a81a6f69c648dd
Tiia <tiia.husa@tuni.fi>
2024-08-15 21:01:03 +0300

    Osa 4 käyttäjäkirjautuminen valmis

--- osa3/part3/controllers/notes.js
+++ osa3/part3/controllers/notes.js
@@ -1,8 +1,13 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => { 
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 }) //Yhdistetään käyttäjätietojen kanssa
+
   response.json(notes)
 })
   
@@ -21,18 +26,36 @@ notesRouter.get('/', async (request, response) => {
     await Note.findByIdAndDelete(request.params.id)
     response.status(204).end()
   })
-  
+
+  //Autentikointitokenin haku
+  const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+      return authorization.replace('Bearer ', '')
+    }
+    return null
+  }
   
   //Post pyyntö MongoDB-tietokantaan
   notesRouter.post('/', async (request, response) => {
     const body = request.body
-  
+    
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
     const note = new Note({
       content: body.content,
       important: body.important || false,
+      user: user._id
     })
   
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+    
     response.status(201).json(savedNote)
   })
   
