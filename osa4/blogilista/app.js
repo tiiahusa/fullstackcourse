9ac74fba7f84b20e77ecb0e2ed1daa427770db1a
Tiia <tiia.husa@tuni.fi>
2024-08-19 07:59:23 +0300

    Blogin postaus vain tokenin kanssa, tehtävät 4.19 asti tehty

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,18 +1,18 @@
-//App.js:ään tietokantayhteydne otto
 const config = require('./utils/config')
 const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
-
-const mongoUrl = process.env.MONGODB_URI
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -31,6 +31,7 @@ mongoose.connect(config.MONGODB_URI)
   
   app.use('/api/blogs', blogsRouter)
   app.use('/api/users', usersRouter)
+  app.use('/api/login', loginRouter)
   
   app.use(middleware.unknownEndpoint)
   app.use(middleware.errorHandler)
