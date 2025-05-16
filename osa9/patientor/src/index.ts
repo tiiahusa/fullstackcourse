568a4b39fc3dbeceda77af033cc5883ba313a653
Tiia <tiia.husa@tuni.fi>
2025-05-16 14:03:36 +0300

    cors

--- osa9/patientor/src/index.ts
+++ osa9/patientor/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+
 import diagnoseRouter from './routes/diagnoses';
 import patientRouter from './routes/patients';
+
 const app = express();
+
 app.use(express.json());
+
+app.use(cors());
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
