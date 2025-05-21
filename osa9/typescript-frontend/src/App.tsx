dfba6f299eb7224f155b01587f360ca1b1fcd50e
Tiia <tiia.husa@tuni.fi>
2025-05-21 07:12:00 +0300

    teht 9.16 ok

--- osa9/typescript-frontend/src/App.tsx
+++ osa9/typescript-frontend/src/App.tsx
@@ -1,8 +1,77 @@
 import type { JSX } from "react";
 
-interface CoursePart {
-  name: string,
-  exerciseCount: number
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic";
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: string[];
+  kind: "special";
+}
+
+type CoursePart = CoursePartSpecial | CoursePartBasic | CoursePartGroup | CoursePartBackground;
+
+const Part = ({ part }: { part: CoursePart }) => {
+  switch(part.kind) {
+    case "basic":
+      return (
+        <div>
+          <p>
+            <b>{part.name} {part.exerciseCount}</b> <br></br>
+            <i>{part.description}</i>
+          </p>
+        </div>
+      )
+    case "group":
+      return (
+        <div>
+          <p>
+            <b>{part.name} {part.exerciseCount}</b><br></br>
+            project exercises {part.groupProjectCount}
+          </p>
+        </div>
+      )    
+    case "background":
+      return (
+        <div>
+          <p>
+            <b>{part.name} {part.exerciseCount}</b><br></br>
+            <i>{part.description}</i><br></br>
+            submit to {part.backgroundMaterial}
+          </p>
+        </div>
+      )
+    case "special":
+      return (
+        <div>
+          <p>
+            <b>{part.name} {part.exerciseCount}</b><br></br>
+            <i>{part.description}</i><br></br>
+            required skills: {part.requirements.map(x => x).join(", ")}
+          </p>
+        </div>
+      )
+  }
+
 }
 
 const Header = ({ name }: { name: string }): JSX.Element => {
@@ -12,10 +81,10 @@ const Header = ({ name }: { name: string }): JSX.Element => {
 const Content = ({ parts }: { parts: CoursePart[] }): JSX.Element => {
   return (
     <div> 
-      {parts.map((part, index) => (
-        <p key={index}>
-          {part.name} {part.exerciseCount}
-        </p>
+      {parts.map((part, key) => (
+        <div key={key}>
+          <Part part={part}></Part>
+        </div>
       ))}
     </div>
   );
@@ -28,20 +97,47 @@ const Total = ({ parts }: { parts: CoursePart[] }): JSX.Element => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
+
   
 
   return (
