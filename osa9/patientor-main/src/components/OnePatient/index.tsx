f0a72ddd5ce5324eca512a8cc54e8d7862b60d97
Tiia <tiia.husa@tuni.fi>
2025-07-09 13:56:46 +0300

    teht 9.22 ok

---
+++ osa9/patientor-main/src/components/OnePatient/index.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { Box, Typography } from '@mui/material';
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
+import TransgenderIcon from '@mui/icons-material/Transgender';
+import { useParams } from 'react-router-dom';
+
+import { Patient } from "../../types";
+
+import patientService from "../../services/patients";
+
+const OnePatient = () => {
+
+    const [patient, setPatient] = useState<Patient>();
+    const id = useParams().id;
+        
+    useEffect(() => {
+        const fetchPatient = async () => {
+        try {
+            if (id) {
+            const fetchedPatient = await patientService.getOne(id);
+            setPatient(fetchedPatient);
+            }
+        } catch (e) {
+            console.error("vituiksi meni ", e);
+        }
+        };
+
+        fetchPatient();
+    }, [id]);
+
+  return (
+    <div className="App">
+      {patient ? (
+        <>
+          <Box>
+            <Typography align="center" variant="h5">
+              {patient.name}{'    '}
+                {patient.gender === 'male' && <MaleIcon />}
+                {patient.gender === 'female' && <FemaleIcon />}
+                {patient.gender != 'female' && patient.gender != 'male' && <TransgenderIcon />}
+            </Typography>
+          </Box>
+          <div>
+            <p>ssh: {patient.ssn}</p>
+            <p>occupation: {patient.occupation}</p>
+          </div>
+        </>
+      ) : (
+        <Typography align="center">Loading...</Typography>
+      )}
+
+    </div>
+  );
+};
+
+export default OnePatient;
