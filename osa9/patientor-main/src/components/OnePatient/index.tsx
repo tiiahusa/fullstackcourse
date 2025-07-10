9e4e31743044d46d03e766463eb209075b5fb053
Tiia <tiia.husa@tuni.fi>
2025-07-10 10:00:14 +0300

    teht 9.24 ok

--- osa9/patientor-main/src/components/OnePatient/index.tsx
+++ osa9/patientor-main/src/components/OnePatient/index.tsx
@@ -20,6 +20,7 @@ const OnePatient = () => {
             if (id) {
             const fetchedPatient = await patientService.getOne(id);
             setPatient(fetchedPatient);
+            console.log(fetchedPatient);
             }
         } catch (e) {
             console.error("vituiksi meni ", e);
@@ -44,6 +45,18 @@ const OnePatient = () => {
           <div>
             <p>ssh: {patient.ssn}</p>
             <p>occupation: {patient.occupation}</p>
+            <br></br>
+            <h3>entries</h3>
+            {patient.entries.map(entry => (
+              <div key={entry.id}>
+                <p>{entry.date} <i>{entry.description}</i></p>
+                {entry.diagnosisCodes?.map( code => (
+                  <ul>
+                    <li>{code}</li>
+                  </ul>
+                ))}
+              </div>
+            ))}
           </div>
         </>
       ) : (
